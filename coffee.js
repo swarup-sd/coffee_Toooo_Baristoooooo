a1940039e6148a032693165fc454453a38fef11a
swarup-sd <81979323+swarup-sd@users.noreply.github.com>
2022-08-04 04:01:00 -0700

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,9 @@
+function product(anything)
+{
+    document.getElementById('imgproduct').src=anything;
+}
+
+function circle(anything)
+{
+    document.getElementById('ccl').style.backgroundColor=anything;
+}
